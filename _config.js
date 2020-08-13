4674a43b6026dfd3c38d89de7941fd604507f16d
Brian Marete <bmarete10@gmail.com>
2020-08-13 12:57:44 +0300

    Create _config.js

--- _config.js
+++ _config.js
@@ -1,9 +1,4 @@
 var config = {}
-
-<<<<<<< HEAD
-// Update to have your correct username and password
-=======
->>>>>>> 76f6fa5 (Create _config.js)
 config.mongoURI = {
     production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
