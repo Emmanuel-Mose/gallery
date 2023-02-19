cfd3ddbb1a87d32b03d08c28c2eec9c545f79a42
Emmanuel-Mose <emmanuel.mose@student.moringaschool.com>
2023-02-19 15:23:27 +0300

    Create _config.js

--- _config.js
+++ _config.js
@@ -1,6 +1,9 @@
 var config = {}
 
+<<<<<<< HEAD
 // Update to have your correct username and password
+=======
+>>>>>>> 76f6fa5 (Create _config.js)
 config.mongoURI = {
     production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
