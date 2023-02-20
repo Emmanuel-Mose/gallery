839528d402a0ca6df917d58c0b91202052f9dc4f
Emmanuel-Mose <emmanuel.mose@student.moringaschool.com>
2023-02-20 18:09:06 +0300

    conflict

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 const config = require('./_config');
 const dotenv= require('dotenv')
-dotenv.config()
+dotenv.config();
 
 // Define routes
 let index = require('./routes/index');
